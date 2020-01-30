b33b328fad1440fb12a7c746a84f20acd0cab5b1
duclocn <duclocn@gmail.com>
2020-01-30 21:11:26 +0700

    Done session 32 - ngContent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { PersonComponent } from './person/person.component';
 import { ListPersonComponent } from './list-person/list-person.component';
 import { ParentComponent } from './parent.component';
 import { ChildComponent } from './child.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { ChildComponent } from './child.component';
     PersonComponent,
     ListPersonComponent,
     ParentComponent,
-    ChildComponent
+    ChildComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,

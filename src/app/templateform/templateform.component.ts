be2363d3f7f6937b6b76a3ef62fedaaaebdd11d7
duclocn <duclocn@gmail.com>
2020-02-08 12:11:28 +0700

    no message

---
+++ src/app/templateform/templateform.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-templateform',
+  templateUrl: './templateform.component.html',
+  styleUrls: ['./templateform.component.css']
+})
+export class TemplateformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(formSignIn) {
+    console.log(formSignIn.value);
+  }
+
+}
